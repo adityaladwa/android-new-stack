3995d378b9eb4addc2ed6155e5b3cc68e87095b9
Aditya Ladwa <ladwa.aditya@gmail.com>
2024-08-27 01:18:02 +0530

    project setup

---
+++ README.md
@@ -0,0 +1,27 @@
+# Android sample project using the new stack
+
+Architecture will follow most of the Google recommended architecture [Link](https://developer.android.com/topic/architecture/recommendations)
+
+
+* Use coroutines or flows for async operations
+* okhttp, retrofit for networking
+* kotlinx.serialization for json serialization
+* Hilt for dependency injection
+* Jetpack compose for UI
+* Use DataStore instead of shared-pref
+* Navigation component for navigation
+* [Multi module app using api impl pattern](https://speakerdeck.com/vrallev/android-at-scale-at-square)
+* Kotlin script instead of groovy
+  * Use toml for versioning libraries
+* Use paging library for showing data (optional)
+* Coil for image loading
+* protobuf or graph-ql?
+
+
+Possible APIs you can use
+
+* https://www.mfapi.in/
+* https://developers.giphy.com/docs/sdk#content-types
+* https://unsplash.com/developers
+* https://cricket-postman.sportmonks.com/#f2147669-38a9-47e3-ae3e-753bda6c1f93
+* https://www.themoviedb.org/
---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.aditya.newstack"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.aditya.newstack"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+    //coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //square
+    implementation(platform(libs.squareup.okhttp.bom))
+    implementation(libs.squareup.okhttp)
+    implementation(libs.squareup.okhttp.logging.interceptor)
+    implementation(libs.squareup.okio)
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.retrofit.converter.kotlinx.serialization)
+
+
+
+
+    //serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+
+    testImplementation(libs.junit)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
