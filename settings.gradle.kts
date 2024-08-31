a9342cc8c79b94b54244bcfc89b83328e0519673
Aditya Ladwa <ladwa.aditya@gmail.com>
2024-08-31 16:40:53 +0530

    moved data and ui modules into core directory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,7 @@ dependencyResolutionManagement {
 rootProject.name = "android-new-stack"
 include(":app")
 include(":data")
+project(":data").projectDir = File(rootDir, "core/data/")
 
 include(":discovery-impl")
 project(":discovery-impl").projectDir = File(rootDir, "features/discovery/discovery-impl/")
@@ -30,3 +31,4 @@ include(":discovery-api")
 project(":discovery-api").projectDir = File(rootDir, "features/discovery/discovery-api/")
 
 include(":ui")
+project(":ui").projectDir = File(rootDir, "core/ui/")
